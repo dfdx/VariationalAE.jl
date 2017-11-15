97ead2a7519af448cd288d52a3a1f08a577e0c45
Andrei Zhabinski <andrei.zhabinski@pandadoc.com>
2017-11-15 17:13:01 +0300

    working MNIST example, cleaned up

--- src/core.jl
+++ src/core.jl
@@ -20,65 +20,15 @@ softplus(x) = log(exp(x) + 1)
 @diffrule softplus(x::Number) x logistic(x) .* ds
 
 
-# variational autoencoder with Gaussian observed and latent variables
-mutable struct VAE{T}
-    # encoder / recognizer
-    We1::AbstractMatrix{T}  # encoder: layer 1 weight
-    be1::AbstractVector{T}  # encoder: layer 1 bias
-    We2::AbstractMatrix{T}  # encoder: layer 2 weight
-    be2::AbstractVector{T}  # encoder: layer 2 bias
-    We3::AbstractMatrix{T}  # encoder: layer 3 mu weight
-    be3::AbstractVector{T}  # encoder: layer 3 mu bias
-    We4::AbstractMatrix{T}  # encoder: layer 3 log(sigma^2) weight
-    be4::AbstractVector{T}  # encoder: layer 3 log(sigma^2) bias
-    # decoder / generator
-    Wd1::AbstractMatrix{T}  # decoder: layer 1 weight
-    bd1::AbstractVector{T}  # decoder: layer 1 bias
-    Wd2::AbstractMatrix{T}  # decoder: layer 2 weight
-    bd2::AbstractVector{T}  # decoder: layer 2 bias
-    Wd3::AbstractMatrix{T}  # decoder: layer 3 weight
-    bd3::AbstractVector{T}  # decoder: layer 3 bias
-end
-
-function Base.show(io::IO, m::VAE{T}) where T
-    print(io, "VAE{$T}($(size(m.We1,2)), $(size(m.We1,1)), $(size(m.We2,1)), " *
-          "$(size(m.We3,1)), $(size(m.Wd1,1)), $(size(m.Wd2,1)), $(size(m.Wd3,1)))")
-end
-
-
-VAE{T}(n_inp, n_he1, n_he2, n_z, n_hd1, n_hd2, n_out) where T =
-    VAE{T}(
-        # encoder
-        xavier_init(n_he1, n_inp),
-        zeros(n_he1),
-        xavier_init(n_he2, n_he1),
-        zeros(n_he2),
-        xavier_init(n_z, n_he2),
-        zeros(n_z),
-        xavier_init(n_z, n_he2),
-        zeros(n_z),
-        # decoder
-        xavier_init(n_hd1, n_z),
-        zeros(n_hd1),
-        xavier_init(n_hd2, n_hd1),
-        zeros(n_hd2),
-        xavier_init(n_out, n_hd2),
-        zeros(n_out)
-    )
-
-
-include("modelopt.jl")
-include("cost.jl")
-
-
-
+include("model.jl")
+include("modelopt.jl")  # TODO: use one from Milk.jl when it's ready
 
 
 model_params(m) = [getfield(m, f) for f in fieldnames(m)]
 model_named_params(m) = [f => getfield(m, f) for f in fieldnames(m)]
 
 
-
+# TODO: use implementation form Milk.jl
 function xavier_init(dim_in, dim_out; c=1)
     low = -c * sqrt(6.0 / (dim_in + dim_out))
     high = c * sqrt(6.0 / (dim_in + dim_out))
@@ -94,24 +44,25 @@ function fit(m::VAE{T}, X::AbstractMatrix{Float64};
     println("Compiling gradient...")
     g = xdiff(vae_cost; m=m, eps=eps, x=x1)
     # fit to data
-    mem = Dict()    
+    mem = Dict()
     m_opt = ModelOptimizer(typeof(m), opt)
     for epoch in 1:n_epochs
-        print("Epoch $epoch")
-        for (i, x) in enumerate(eachbatch(X, size=batch_size))            
-            eps = zeros(size(m.We3, 1), batch_size)
-            cost, dm, deps, dx = Base.invokelatest(g, m, eps, x)
+        print("Epoch $epoch: ")
+        epoch_cost = 0
+        t = @elapsed for (i, x) in enumerate(eachbatch(X, size=batch_size))
+            eps = typeof(x1)(rand(Normal(0, 1), size(m.We3, 1), batch_size))
+            cost, dm, deps, dx = Base.invokelatest(g, m, eps, x, mem)
             update_params!(m_opt, m, dm)
-            println("  batch $i, cost = $cost")
-        end        
+            epoch_cost += cost
+        end
+        println("avg_cost=$(epoch_cost / (size(X,2) / batch_size)), elapsed=$t")
     end
+    return m
 end
 
 
 function generate(m::VAE, z::AbstractVector)
-    hd1 = tanh.(m.Wd1 * z .+ m.bd1)
-    hd2 = tanh.(m.Wd2 * hd1 .+ m.bd2)
-    return logistic.(m.Wd3 * hd2 .+ m.bd3)
+    return decode(m, z)
 end
 
 
@@ -126,35 +77,25 @@ end
 
 function reconstruct(m::VAE, x::AbstractVector)
     x = reshape(x, length(x), 1)
-    he1 = tanh.(m.We1 * x .+ m.be1)
-    he2 = tanh.(m.We2 * he1 .+ m.be2)
-    mu = m.We3 * he2 .+ m.be3
-    log_sigma2 = m.We4 * he2 .+ m.be4
-    z = mu .+ sqrt.(exp.(log_sigma2))
-    hd1 = tanh.(m.Wd1 * z .+ m.bd1)
-    hd2 = tanh.(m.Wd2 * hd1 .+ m.bd2)
-    x_rec = logistic.(m.Wd3 * hd2 .+ m.bd3)
+    mu, _ = encode(m, x)
+    z = mu
+    x_rec = decode(m, z)
     return x_rec
 end
 
+
+## experiments
 
 function showit(x)
     reshape(x, 28, 28) |> imshow
 end
 
 
-
-#################################################
-
-function load_espresso()
-    for n in Base.names(Espresso, true) @eval import Espresso: $n end
+function show_both(m, x)
+    x_ = reconstruct(m, x)
+    showit(x)
+    showit(x_)
 end
-
-
-function load_xgrad()
-    for n in Base.names(XGrad, true) @eval import XGrad: $n end
-end
- 
 
 
 
@@ -162,7 +103,7 @@ function run()
     m = VAE{Float64}(784, 500, 500, 20, 500, 500, 784)
     X, _ = MNIST.traindata()
     X = reshape(X, 784, 60000)
-    @time fit(m, X; n_epochs=10)
+    @time m = fit(m, X; n_epochs=2)
 
     reconstruct(m, X[:, 100]) |> showit
     for i=1:5
